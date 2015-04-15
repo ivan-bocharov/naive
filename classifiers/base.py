585a78a0be2387f38a41701e5a25abfae2e5bbc8
Ivan Bocharov <bocharovia@gmail.com>
2015-04-15 21:59:34 +0300

    Added skeletons of classifier classes

--- classifiers/base.py
+++ classifiers/base.py
@@ -1,2 +1,12 @@
 # encoding=utf-8
 #author: Bocharov Ivan
+
+
+class BaseClassifier(object):
+
+    def fit(self, X_train, y_train):
+        raise NotImplementedError()
+
+    def predict(self, X_test):
+        raise NotImplementedError
+
--- classifiers/naive.py
+++ classifiers/naive.py
@@ -1,2 +1,16 @@
 # encoding=utf-8
 #author: Bocharov Ivan
+
+from base import BaseClassifier
+
+
+class NaiveBayesClassifier(BaseClassifier):
+
+    def __init__(self):
+        pass
+
+    def fit(self, X_train, y_train):
+        pass
+
+    def predict(self, y_train):
+        pass
