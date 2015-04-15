77d8b3c10ad20f5b3dd8d54b8b5b7cbbb43d65a8
Ivan Bocharov <bocharovia@gmail.com>
2015-04-15 22:23:50 +0300

    Added some stuff to gitignore

--- classifiers/naive.py
+++ classifiers/naive.py
@@ -1,15 +1,19 @@
 # encoding=utf-8
 #author: Bocharov Ivan
 
+import numpy as np
+
 from base import BaseClassifier
 
 
 class NaiveBayesClassifier(BaseClassifier):
 
-    def __init__(self):
+    def __init__(self, alpha=1):
+        self.alpha = 0
         pass
 
     def fit(self, X_train, y_train):
+        print X_train.shape()
         pass
 
     def predict(self, y_train):
