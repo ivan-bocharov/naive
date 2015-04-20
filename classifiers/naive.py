9e2b90dca03990c02167cbf183fc6c34bdd92001
Ivan Bocharov <bocharovia@gmail.com>
2015-04-20 07:40:20 +0300

    Removed unused print statements and excessive comments

--- classifiers/naive.py
+++ classifiers/naive.py
@@ -7,10 +7,11 @@ import operator
 
 from base import BaseClassifier
 from math import log
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
-from collections import defaultdict
+from sklearn.datasets import make_blobs
 
+from collections import defaultdict
+from scipy.sparse import issparse
+import matplotlib.pyplot as plt
 
 
 class NaiveBayesClassifier(BaseClassifier):
@@ -23,13 +24,17 @@ class NaiveBayesClassifier(BaseClassifier):
         self.feature_counts = defaultdict(lambda: defaultdict(lambda: 0))
 
     def fit(self, X_train, y_train):
-        for i in xrange(y_train.shape[0]):
+
+        for i in xrange(len(y_train)):
             label = y_train[i]
             self.labels[label] += 1
-            for index in X_train[i].indices:
-                self.feature_counts[label][index] += X_train[i][0, index]
-            self.labelsfeatures[label] += len(X_train[i].indices)
+            features = X_train[i].indices if issparse(X_train) else X_train[i]
+            features /= np.linalg.norm(features)
+            for j in xrange(len(features)):
+                self.feature_counts[label][j] += X_train[i, j]
+            self.labelsfeatures[label] += len(X_train[i])
         print self.labels
+        print self.labelsfeatures
 
     def predict(self, X):
         prediction = [self.predict_one(row) for row in X]
@@ -37,9 +42,10 @@ class NaiveBayesClassifier(BaseClassifier):
 
     def predict_one(self, obj):
         probabilities = {}
+        obj /= np.linalg.norm(obj)
         for label in self.labels:
             probabilities[label] = self.object_probability(obj, label) + \
-                                   self.label_probability(label)
+                self.label_probability(label)
         return max(probabilities.iteritems(), key=operator.itemgetter(1))[0]
 
     def feature_count(self, feature, label):
@@ -59,23 +65,24 @@ class NaiveBayesClassifier(BaseClassifier):
             raise KeyError
 
     def get_vocabulary_size(self):
-        temp = defaultdict(lambda: 1)
+        feature_set = set()
         for label in self.feature_counts:
             for feature in self.feature_counts[label]:
-                temp[feature];
-        self.vocabulary_size = len(temp)
+                feature_set.add(feature)
+        self.vocabulary_size = len(feature_set)
 
     def get_probability(self, feature, label):
+        key, value = feature
         if label in self.labels:
-            return (self.feature_count(feature, label)+self.alpha)/(self.labelsfeatures[label]+self.vocabulary_size*self.alpha)
+            return (self.feature_count(key, label)+self.alpha)/(self.labelsfeatures[label]+self.vocabulary_size*self.alpha)
         else:
             print "Given label doesn't exist"
             raise KeyError
 
     def object_probability(self, row_object, label):
         probability = 0.0
-        for feature in row_object.indices:
-            probability += log(self.get_probability(feature, label))
+        for feature in xrange(len(row_object)):
+            probability += row_object[feature]*log(self.get_probability((feature, row_object[feature]), label))
         return probability
 
     def label_probability(self, label):
@@ -85,32 +92,32 @@ class NaiveBayesClassifier(BaseClassifier):
             print "Given label doesn't exist"
             raise KeyError
 
-
-
 if __name__ == '__main__':
-    data_train = fetch_20newsgroups(subset='train',
-                                shuffle=True, random_state=42,
-                                )
 
-    data_test = fetch_20newsgroups(subset='test',
-                               shuffle=True, random_state=42,
-                               )
-    print('data loaded')
+    X, y = make_blobs(n_samples=1500, centers=5, n_features=20, center_box= (5.0, 10.0))
+    X_train, X_test = X[:1000], X[1000:]
+    y_train, y_test = y[:1000], y[1000:]
 
-    categories = data_train.target_names
 
-    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                                 stop_words='english')
-    y_train, y_test = data_train.target, data_test.target
+    # plt.scatter(X[:, 0], X[:, 1], marker='o', c=y)
+    # plt.show()
 
-    X_train = vectorizer.fit_transform(data_train.data)
-
-    X_test = vectorizer.transform(data_test.data)
-
-    nb = NaiveBayesClassifier()
+    nb = NaiveBayesClassifier(0.1)
     print('Fitting...')
     nb.fit(X_train, y_train)
     print('Fitted!')
     nb.get_vocabulary_size()
-    print nb.predict(X_test)
+    res = nb.predict(X_test)
     print('Done!')
+
+    from sklearn.metrics import f1_score
+
+    print f1_score(y_test, res, average='macro')
+
+
+    from sklearn.naive_bayes import MultinomialNB
+
+    mnb = MultinomialNB(alpha=0.1)
+    mnb.fit(X_train, y_train)
+    m_res = mnb.predict(X_test)
+    print f1_score(y_test, m_res, average='micro')
