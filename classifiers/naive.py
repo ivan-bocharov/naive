a36d6bd2a80cd7680c7685699c7605804b377526
Ivan Bocharov <bocharovia@gmail.com>
2015-04-24 20:18:21 +0200

    Some renaming in the NB class

--- classifiers/naive.py
+++ classifiers/naive.py
@@ -7,11 +7,9 @@ import operator
 
 from base import BaseClassifier
 from math import log
-from sklearn.datasets import make_blobs
 
 from collections import defaultdict
 from scipy.sparse import issparse
-import matplotlib.pyplot as plt
 
 
 class NaiveBayesClassifier(BaseClassifier):
@@ -20,20 +18,20 @@ class NaiveBayesClassifier(BaseClassifier):
         self.alpha = alpha
 
     def initiate_priors(self):
-        self.labels = defaultdict(lambda: 0)
-        self.labelsfeatures = defaultdict(lambda: 0)
+        self.total_documents_in_label = defaultdict(lambda: 0)
+        self.total_features_in_label = defaultdict(lambda: 0)
         self.feature_counts = defaultdict(lambda: defaultdict(lambda: 0))
 
     def fit(self, X_train, y_train):
         self.initiate_priors()
         for i in xrange(len(y_train)):
             label = y_train[i]
-            self.labels[label] += 1
+            self.total_documents_in_label[label] += 1
             features = X_train[i].indices if issparse(X_train) else X_train[i]
             features /= np.linalg.norm(features)
             for j in xrange(len(features)):
                 self.feature_counts[label][j] += X_train[i, j]
-            self.labelsfeatures[label] += len(X_train[i])
+            self.total_features_in_label[label] += len(X_train[i])
         self.vocabulary_size = X_train.shape[1]
 
     def predict(self, X):
@@ -43,7 +41,7 @@ class NaiveBayesClassifier(BaseClassifier):
     def predict_one(self, obj):
         probabilities = {}
         obj /= np.linalg.norm(obj)
-        for label in self.labels:
+        for label in self.total_documents_in_label:
             probabilities[label] = self.object_probability(obj, label) + \
                 self.label_probability(label)
         return max(probabilities.iteritems(), key=operator.itemgetter(1))[0]
@@ -55,32 +53,30 @@ class NaiveBayesClassifier(BaseClassifier):
             else:
                 return 0
 
-    def total_label_count(self, label):
-        return sum(self.feature_counts[label].values())
-
     def label_count(self, label):
-        if label in self.labels:
-            return self.labels[label]
+        if label in self.total_documents_in_label:
+            return self.total_documents_in_label[label]
         else:
             raise KeyError
 
     def get_probability(self, feature, label):
         key, value = feature
-        if label in self.labels:
-            return (self.feature_count(key, label)+self.alpha)/(self.labelsfeatures[label]+self.vocabulary_size*self.alpha)
+        if label in self.total_documents_in_label:
+            return (self.feature_count(key, label)+self.alpha)/(self.total_features_in_label[label]+self.vocabulary_size*self.alpha)
         else:
             print "Given label doesn't exist"
             raise KeyError
 
-    def object_probability(self, row_object, label):
+    def object_probability(self, classification_object, label):
         probability = 0.0
-        for feature in xrange(len(row_object)):
-            probability += row_object[feature]*log(self.get_probability((feature, row_object[feature]), label))
+        for feature in xrange(len(classification_object)):
+            probability += classification_object[feature]*log(self.get_probability(
+                (feature, classification_object[feature]), label))
         return probability
 
     def label_probability(self, label):
-        if label in self.labels:
-            return log(self.labels[label]/sum(self.labels.values()))
+        if label in self.total_documents_in_label:
+            return log(self.total_documents_in_label[label]/sum(self.total_documents_in_label.values()))
         else:
             print "Given label doesn't exist"
             raise KeyError
